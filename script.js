81df23854a7df64609e13b6a2c3fa0d77a64c837
Ollie <ollie.bhm@gmail.com>
2018-09-19 19:38:54 +0100

    Get start button working once

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log('hello world!')
+
+var context = new (window.AudioContext || window.webkitAudioContext)();
+var oscillator;
+
+var initializeOscillator = function(){
+  oscillator = context.createOscillator();
+  window.oscillator = oscillator
+
+  oscillator.type = 'sine';
+  oscillator.frequency.value = 440;
+  oscillator.connect(context.destination);
+}
+
+
+$(document).on('click', '#toggle', function(event){
+  if ($(this).data().active) {
+    $(this).data('active', 'false')
+    oscillator.stop()
+  } else {
+    if (oscillator == undefined) {
+      initializeOscillator();
+    }
+    $(this).data('active', 'true')
+    oscillator.start();
+  }
+})
+
+
