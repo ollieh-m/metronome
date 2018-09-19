e807d7cae79a9661221d452b77432af2610934a8
Craig Coles <craig@simpleweb.co.uk>
2018-09-19 20:08:41 +0100

    Bleep bleep bleep bloop

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-console.log('hello world!')
+let interval;
 
-var context = new (window.AudioContext || window.webkitAudioContext)();
-var oscillator;
+const startCycle = () => {
+  const tempo = $('.tempo').val();
+  const ms = 60000 / tempo;
 
-var initializeOscillator = function(){
-  oscillator = context.createOscillator();
-  window.oscillator = oscillator
+  window.AudioContext = window.AudioContext || window.webkitAudioContext;
+  const context = new window.AudioContext();
 
-  oscillator.type = 'sine';
-  oscillator.frequency.value = 440;
-  oscillator.connect(context.destination);
+  interval = setInterval(() => {
+    const oscillator = context.createOscillator();
+    const currentTime = context.currentTime;
+
+    oscillator.type = 'sine';
+    oscillator.frequency.value = 480;
+    oscillator.connect(context.destination);
+
+    oscillator.start(currentTime);
+    oscillator.stop(currentTime + 0.1);
+
+  }, ms);
 }
-
 
 $(document).on('click', '#toggle', function(event){
   if ($(this).data().active) {
     $(this).text("Start")
     $(this).data('active', false);
-    oscillator.stop()
+    clearInterval(interval);
   } else {
-    initializeOscillator();
     $(this).text("Stop");
     $(this).data('active', true)
-    oscillator.start();
+    startCycle();
   }
 })
