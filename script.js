106bffec821df475dbf0b897d7f361b1a16657c2
Craig Coles <craig@simpleweb.co.uk>
2018-09-19 20:22:02 +0100

    Get number of bars working

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 let interval;
 
 const startCycle = () => {
+
   const tempo = $('.tempo').val();
+  let beeps = $('.bars').val()*4;
   const ms = 60000 / tempo;
 
   window.AudioContext = window.AudioContext || window.webkitAudioContext;
   const context = new window.AudioContext();
 
   interval = setInterval(() => {
+    if (beeps === 0) {
+      $('#toggle').text("Start")
+      $('#toggle').data('active', false);
+      clearInterval(interval);
+      return false;
+    }
+
     const oscillator = context.createOscillator();
     const currentTime = context.currentTime;
 
     oscillator.type = 'sine';
-    oscillator.frequency.value = 480;
+    oscillator.frequency.value = beeps % 4 ? 480 : 600;
     oscillator.connect(context.destination);
 
     oscillator.start(currentTime);
     oscillator.stop(currentTime + 0.1);
 
+    beeps--;
   }, ms);
 }
 
